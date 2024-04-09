eb77f6cb3cced577dd0895f8d2212b0e748310b3
nisargsharma01 <166279671+nisargsharma01@users.noreply.github.com>
2024-04-09 19:56:03 +0530

    update 5

--- project/project 1/project1.js
+++ project/project 1/project1.js
@@ -1,1 +1,6 @@
 
+const startButton = document.gelElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+
+const questionContainerElement = document.getElementById('question-container')
+
